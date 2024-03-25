3bd125bcf04d632818b7275022bc5d230257a1f1
Brohelloforgem <146630422+Brohelloforgem@users.noreply.github.com>
2024-03-25 11:21:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import discord
+import asyncio
+import replicate
+
+intents = discord.Intents.all()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+    try:
+        with open('avatar.gif', 'rb') as avatar:
+            await client.user.edit(avatar=avatar.read())
+        print('Animated avatar uploaded successfully!')
+    except Exception as e:
+        print('Failed to upload animated avatar:', e)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('&hey'):
+        await message.channel.send('Hey, the  code is working fine')
+
+DISCORD_BOT_TOKEN = 'MTIwNjU0ODQ5MDgzOTEzNDI0OA.GdVV96.JlZh_DGzGfd6RjnKHNw7nj6mkp3sCDva5pw01U'
+
+client.run(DISCORD_BOT_TOKEN)
